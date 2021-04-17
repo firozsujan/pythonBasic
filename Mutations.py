0a0975a54a59b8e515a8b1eeee7ac7ef8b837b59
Firoz Ahmed Talukder <44744161+firozsujan@users.noreply.github.com>
2021-04-17 16:37:03 +0600

    HackerRank 15

---
+++ Mutations.py
@@ -0,0 +1,15 @@
+# Task # hackerRank # Mutations
+# https://www.hackerrank.com/challenges/python-mutations/problem
+
+def mutate_string(string, position, character):
+    newString = ""
+    l = list(string)
+    l[position] = character
+    string = ''.join(l)
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
